c372e0ef7929e527c701a587b9aab9cb4bb2bf59
mohamedazizJellazi <aziz.jellazi@gmail.com>
2024-03-14 06:11:43 +0100

    jenkinsfile back v6 testing correction

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forFeature([
+          {
+            name: 'User',
+            schema: UserSchema,
+          },
+        ]),
+        JwtModule.register({ secret: 'aziz' }),
+      ],
       controllers: [UserController],
       providers: [UserService],
     }).compile();
--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,11 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forFeature([
+          {
+            name: 'User',
+            schema: UserSchema,
+          },
+        ]),
+        JwtModule.register({ secret: 'aziz' }),
+      ],
       providers: [UserService],
     }).compile();
 
